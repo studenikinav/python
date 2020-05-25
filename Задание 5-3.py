081550794847892ccd160afd95be4e0970f3c14d
StudenikinAV <avstudenikin@gmail.com>
2020-05-26 00:29:32 +0300

    ДЗ по 5-у уроку

---
+++ Задание 5-3.py
@@ -0,0 +1,11 @@
+with open('text-5-3.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    print(my_list)
+#     for i in my_list:
+#         i = i.split()
+#         if int(i[1]) < 20000:
+#             poor.append(i[0])
+#         sal.append(i[1])
+# print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
